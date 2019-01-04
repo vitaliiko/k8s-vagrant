f2fbc59e5d520cfe3d8dcb2a2d69aafe9b442130
Vitalii Kravchenko <v.kravchenko@spd-ukraine.com>
2019-01-04 19:26:09 +0200

    Add some updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,20 +8,30 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update && apt-get install -y docker-ce
 systemctl enable docker && systemctl start docker
 
-echo "*** Install kubelet kubeadm kubectl ***"
+echo "*** Install kubernetes tools ***"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update && apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+apt-get update && apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 
-# Set Root password
 echo "*** Set root password ***"
 echo -e "kubeadmin\nkubeadmin" | passwd 2>/dev/null
 
-# Enable ssh password authentication
 echo "*** Enable ssh password authentication ***"
 sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/^PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl reload sshd
+
+echo "*** Set kubelet node IP to private network IP"
+IPADDR=$(ip a show enp0s8 | grep inet | grep -v inet6 | awk '{print $2}' | cut -f1 -d/)
+sed -i "/KUBELET_EXTRA_ARGS=/c\KUBELET_EXTRA_ARGS=--node-ip=$IPADDR" /etc/default/kubelet
+systemctl daemon-reload
+systemctl restart kubelet
+
+echo "*** Install ZSH ***"
+apt-get install -y zsh git
+su - vagrant -c 'wget -q https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh && chmod +x install.sh'
+su - vagrant -c 'echo vagrant | ./install.sh && rm -f install.sh'
+su - vagrant -c "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"3den\"/g' ~/.zshrc"
+su - vagrant -c 'echo '"'"'PROMPT="$fg_bold[blue]%}%m ${PROMPT}"'"'"' >> ~/.zshrc'
--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 echo "*** Initialize Kubernetes Cluster ***"
-kubeadm init --apiserver-advertise-address=172.42.42.100 --pod-network-cidr=10.244.0.0/16
+IPADDR=$(ip a show enp0s8 | grep inet | grep -v inet6 | awk '{print $2}' | cut -f1 -d/)
+kubeadm init --apiserver-advertise-address=$IPADDR --pod-network-cidr=10.244.0.0/16
 
 echo "*** Copy kube admin config to Vagrant user .kube directory ***"
 mkdir /home/vagrant/.kube
 cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown -R vagrant:vagrant /home/vagrant/.kube
 
-echo "*** Deploy flannel network ***"
-su - vagrant -c "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+echo "*** Deploy pods network ***"
+su - vagrant -c 'kubectl apply -f /vagrant/kube-flannel.yml'
+#su - vagrant -c "kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
+#su - vagrant -c "kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')""
+
+echo "*** Setting up Kubernetes Dashboard ***"
+su - vagrant -c 'kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml'
 
 echo "*** Generate and save cluster join command to ~/joincluster.sh ***"
 kubeadm token create --print-join-command > /root/joincluster.sh
